9832860d8743d216f8a20364da9e68ed018c2e7b
Christopher L Plaisier, PhD <plaisier@asu.edu>
2024-09-18 12:59:38 -0700

    Updating to qG0

--- tests/predict_samples.py
+++ tests/predict_samples.py
@@ -38,7 +38,7 @@ PCW8 = sc.read_h5ad('../data/W8-1_normalized_ensembl.h5ad')
 PCW8_labels = ccAFv2.predict_labels(PCW8, species='human', gene_id='ensembl')
 
 # Save into scanpy object
-PCW8.obs['ccAFv2'] = pd.Categorical(PCW8_labels[0], categories=['Neural G0', 'G1', 'Late G1', 'S', 'S/G2', 'G2/M', 'M/Early G1', 'Unknown'], ordered=True)
+PCW8.obs['ccAFv2'] = pd.Categorical(PCW8_labels[0], categories=['qG0', 'G1', 'Late G1', 'S', 'S/G2', 'G2/M', 'M/Early G1', 'Unknown'], ordered=True)
 
 # Run UMAP of U5 hNSCs
 sc.pp.highly_variable_genes(PCW8, n_top_genes=2000)
@@ -47,7 +47,7 @@ sc.pp.neighbors(PCW8)
 sc.tl.umap(PCW8)
 
 # Dictionary to provdie colors to plotting
-cmap1 = {"Neural G0": "#d9a428", "G1": "#f37f73", "Late G1": "#1fb1a9",  "S": "#8571b2", "S/G2": "#db7092", "G2/M": "#3db270" ,"M/Early G1": "#6d90ca",  "Unknown": "#d3d3d3"}
+cmap1 = {"qG0": "#d9a428", "G1": "#f37f73", "Late G1": "#1fb1a9",  "S": "#8571b2", "S/G2": "#db7092", "G2/M": "#3db270" ,"M/Early G1": "#6d90ca",  "Unknown": "#d3d3d3"}
 
 # Plot UMAP of U5 hNSCs
 sc.pl.umap(PCW8, color=['ccAFv2'], palette=cmap1, save='ccAFv2_UMAP_PCW8.pdf')
