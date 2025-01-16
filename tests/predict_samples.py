3664e45e4c7667edd67aa4e0129ee17d60517971
Christopher L Plaisier, PhD <plaisier@asu.edu>
2025-01-16 09:10:17 -0700

    Updated testing to test out include_g0 parameter.

--- tests/predict_samples.py
+++ tests/predict_samples.py
@@ -23,21 +23,31 @@
 ## Load Python packages for classifiers ##
 ##########################################
 
-# General
 import pandas as pd
 import scanpy as sc
 import ccAFv2
 
 
-#####################
-## Test prediction ##
-#####################
+##############################################
+## Dictionary to provdie colors to plotting ##
+##############################################
+cmap1 = {"G0/G1": "#e34234", "Neural G0": "#d9a428", "G1": "#f37f73", "Late G1": "#1fb1a9",  "S": "#8571b2", "S/G2": "#db7092", "G2/M": "#3db270" ,"M/Early G1": "#6d90ca",  "Unknown": "#d3d3d3"}
 
-# Load up test dataset
+
+###########################
+## Single cell or nuclei ##
+###########################
+
+# Load up test dataset from Zeng et al., 2023 (PMID = 37192616)
 PCW8 = sc.read_h5ad('../data/W8-1_normalized_ensembl.h5ad')
 
+
+################################
+## Predict with out Neural G0 ##
+################################
+
 # Run ccAFv2 to predict cell labels
-PCW8_labels = ccAFv2.predict_labels(PCW8, threshold=0.9, species='human', gene_id='ensembl')
+PCW8_labels = ccAFv2.predict_labels(PCW8, species='human', gene_id='ensembl', include_g0=False)
 
 # Save into scanpy object
 PCW8.obs['ccAFv2'] = PCW8_labels[0]
@@ -48,9 +58,26 @@ sc.tl.pca(PCW8)
 sc.pp.neighbors(PCW8)
 sc.tl.umap(PCW8)
 
-# Dictionary to provdie colors to plotting
-cmap1 = {"G0/G1": "#e34234", "Neural G0": "#d9a428", "G1": "#f37f73", "Late G1": "#1fb1a9",  "S": "#8571b2", "S/G2": "#db7092", "G2/M": "#3db270" ,"M/Early G1": "#6d90ca",  "Unknown": "#d3d3d3"}
+# Plot UMAP of U5 hNSCs
+sc.pl.umap(PCW8, color=['ccAFv2'], palette=cmap1, save='ccAFv2_UMAP_PCW8_noG0.pdf')
+
+
+############################
+## Predict with Neural G0 ##
+############################
+
+# Run ccAFv2 to predict cell labels
+PCW8_labels = ccAFv2.predict_labels(PCW8, species='human', gene_id='ensembl', include_g0=True)
+
+# Save into scanpy object
+PCW8.obs['ccAFv2'] = PCW8_labels[0]
+
+# Run UMAP of U5 hNSCs
+sc.pp.highly_variable_genes(PCW8, n_top_genes=2000)
+sc.tl.pca(PCW8)
+sc.pp.neighbors(PCW8)
+sc.tl.umap(PCW8)
 
 # Plot UMAP of U5 hNSCs
-sc.pl.umap(PCW8, color=['ccAFv2'], palette=cmap1, save='ccAFv2_UMAP_PCW8.pdf')
+sc.pl.umap(PCW8, color=['ccAFv2'], palette=cmap1, save='ccAFv2_UMAP_PCW8_wG0.pdf')
 
